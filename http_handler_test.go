9ab67215348fc2249cd56ade34c840a9c33e24fe
Alexander Trakhimenok <alex@trakhimenok.com>
2025-02-18 23:54:27 +0000

    fix: TestHttpHandlerForAppEngine fixed

--- http_handler_test.go
+++ http_handler_test.go
@@ -8,24 +8,15 @@ import (
 
 func TestHttpHandlerForAppEngine(t *testing.T) {
 
-	loggersCreated := 0
-	newLogger := func(logID string, opts ...logging.LoggerOption) *logging.Logger {
-		loggersCreated++
-		return &logging.Logger{}
-	}
-
 	handlerCalled := 0
 	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		handlerCalled++
 	}
 
-	wrappedHandler := HttpHandlerForAppEngine(handler, newLogger)
+	wrappedHandler := HttpHandlerForAppEngine(handler, &logging.Logger{})
 	if wrappedHandler == nil {
 		t.Error("Expected handler to be created, but got nil")
 
-	}
-	if loggersCreated != 1 {
-		t.Errorf("Expected 1 logger to be created, but got %v", loggersCreated)
 	}
 	if handlerCalled != 0 {
 		t.Errorf("Expected handler to be called 0 times, but got %v", handlerCalled)
